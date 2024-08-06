42e43766ad0149a788d00ab251c014388bdffee4
Voroninaka <voroninaka@alderasoft.ru>
2024-08-06 17:48:48 +0700

    Add KeyboardTrait.php

--- app/Traits/KeyboardTrait.php
+++ app/Traits/KeyboardTrait.php
@@ -3,12 +3,14 @@
 namespace App\Traits;
 
 use Illuminate\Database\Eloquent\Collection;
+use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardButton;
+use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardMarkup;
 use SergiX44\Nutgram\Telegram\Types\Keyboard\KeyboardButton;
 use SergiX44\Nutgram\Telegram\Types\Keyboard\ReplyKeyboardMarkup;
 
 trait KeyboardTrait
 {
-    public function createKeyboardFormCollection(Collection $collection): ReplyKeyboardMarkup
+    public function createReplyKeyboardFormCollection(Collection $collection): ReplyKeyboardMarkup
     {
         $keyboardButtonArray = [];
         foreach ($collection as $item) {
@@ -17,4 +19,16 @@ trait KeyboardTrait
 
         return ReplyKeyboardMarkup::make()->addRow($keyboardButtonArray);
     }
+
+
+    public function createInlineKeyboardFormCollection($collection, $callbackName): InlineKeyboardMarkup
+    {
+        $keyboardButtonArray = [];
+        foreach ($collection as $item) {
+            $keyboardButtonArray[] = InlineKeyboardButton::make($item->name, callback_data: "$callbackName:$item->id");
+        }
+
+        return InlineKeyboardMarkup::make()->addRow($keyboardButtonArray);
+    }
+
 }
