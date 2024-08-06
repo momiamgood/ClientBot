c7c2d6927a01c7f84671ea2fff041872b60b39fe
Voroninaka <voroninaka@alderasoft.ru>
2024-08-06 17:47:06 +0700

    Update db

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
     use HasFactory;
 
+    const STATUS_NEW = 1;
+    const STATUS_IN_PROGRESS = 2;
     protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2024_08_03_180229_create_orders_table.php
+++ database/migrations/2024_08_03_180229_create_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->integer('status');
-            $table->text('description');
+            $table->text('text');
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
